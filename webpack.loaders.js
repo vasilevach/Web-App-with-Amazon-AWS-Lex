b98006c97e6fecbbd9ce2ec19f2fec76ae90c34b
John Michael Ferraris <johnmichaelferraris@gmail.com>
2017-12-28 14:01:58 +0800

    Added query presets to fix transpile error

--- webpack.loaders.js
+++ webpack.loaders.js
@@ -2,7 +2,10 @@ module.exports = [
   {
     test: /\.jsx?$/,
     exclude: /(node_modules|bower_components|public\/)/,
-    loader: "babel-loader"
+    loader: "babel-loader",
+    query: {
+      presets: ['es2015', 'react']
+    }
   },
   {
     test: /\.css$/,
